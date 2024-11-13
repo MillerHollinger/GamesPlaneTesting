332d1b58ae3120acb37c81fac29451e763239eb1
Miller Hollinger <hollingermiller@gmail.com>
2024-11-13 14:46:15 -0800

    Piece model

---
+++ HaarTest.py
@@ -0,0 +1,22 @@
+# From https://medium.com/analytics-vidhya/haar-cascades-explained-38210e57970d
+
+import numpy as np
+import cv2
+f_cascade = cv2.CascadeClassifier("face.xml")
+e_cascade = cv2.CascadeClassifier("eye.xml")
+
+img = cv2.imread("") # TODO Add image here
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = f_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = e_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
