902c52b4b8a8b45c6d9b0af92dc6db81e4f6b117
Miller Hollinger <hollingermiller@gmail.com>
2024-12-03 18:43:11 -0800

    First cascade trained

--- HaarTest.py
+++ HaarTest.py
@@ -2,20 +2,16 @@
 
 import numpy as np
 import cv2
-f_cascade = cv2.CascadeClassifier("face.xml")
-e_cascade = cv2.CascadeClassifier("eye.xml")
+piece_cascade = cv2.CascadeClassifier("HaarTraining/classifier/cascade.xml")
 
-img = cv2.imread("") # TODO Add image here
+img = cv2.imread("HaarTesting/p1.jpg")
+img = cv2.resize(img, (0,0), fx=0.08, fy=0.08) 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = f_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
+pieces = piece_cascade.detectMultiScale(gray, 1.3, 5)
+print(len(pieces))
+for (x,y,w,h) in pieces:
     img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = e_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
