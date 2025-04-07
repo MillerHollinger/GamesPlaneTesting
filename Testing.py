cfc30bb3728d577ca4e451dddd31decde7ad4db1
Josh Zhang <joshzhang@wifi-10-40-195-211.wifi.berkeley.edu>
2025-04-06 20:00:19 -0700

    update

--- Testing.py
+++ Testing.py
@@ -1,38 +1,66 @@
-# Entry point for testing the system.
-
+import cv2
+import yaml
+import time
 from Games.DummyGame import *
 
-# Create an instance of the game.
-game = DummyGame("./example-calibration.yaml")
-print(f"Starting instance of {game.name}")
+YAML = "josh_calibration.yaml"
 
-# Read in the picture.
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="Path to image with ARUCO tag(s).")
+ap.add_argument("-v", "--video", type=str, required=True,
+	help="Path to video with ARUCO tag(s)")
+ap.add_argument("-s", "--scale", type=float, default=1.0,
+	help="Scale factor s*(x,y) for frames")
+ap.add_argument("-d", "--delay", type=int, default=1,
+	help="Delay (ms) between frames shown")
+
 args = vars(ap.parse_args())
-print("Processing image " + args["image"])
+name = args["video"]
+scale= args["scale"]
+print("Processing video " + name)
 
-# Rescale the image.
-# Note: At high image sizes, accuracy lowers.
-IMAGE_SCALE = 400
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=IMAGE_SCALE)
-
-# Get the DigitalArucos from this picture.
-pieces, anchors = game.process_image(image)
-
-for info in pieces + anchors:
-    info.put_summary_graphic(image)
-    info.put_bounds(image)
-
-to_board = game.to_board(pieces)
-for y in range(4, -1, -1):
-    result = ""
-    for x in range(0, 5):
-        result = result + str(to_board[x][y])
-    print(result)
+if scale != 1.0:
+    with open(YAML, "r") as file:
+        calib = yaml.safe_load(file)
+    
+    cam = calib["camera_matrix"]
+    cam[0][0] *= scale  # f_x
+    cam[1][1] *= scale  # f_y
+    cam[0][2] *= scale  # c_x
+    cam[1][2] *= scale  # c_y
+    
+    YAML = f"scaled_{scale}_{YAML}"
+    with open(YAML, "w") as file:
+        yaml.dump(calib, file)
 
 
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+game = DummyGame(YAML)
+print(f"Starting instance of {game.name}")
+cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+video = cv2.VideoCapture(name)
+times = []
+
+while True:
+    start_time = time.time()                # TIMER START
+    ret, image = video.read()
+    if not ret: break
+    if scale != 1.0:
+        image = cv2.resize(image, None, fx=scale, fy=scale)
+
+    pieces, anchors = game.process_image(image)
+    for info in pieces + anchors:
+        info.put_summary_graphic(image)
+        info.put_bounds(image)
+    # print(game.to_board(pieces))
+    
+    cv2.imshow(name, image)
+    cv2.waitKey(args["delay"])
+    times.append(time.time() - start_time)  # TIMER END
+
+video.release()
+cv2.destroyAllWindows()
+
+avg_time = sum(times) / len(times)
+print(f"Total time  : {sum(times):.3f} secs")
+print(f"Total frames: {len(times)} frames")
+print(f"Average time: {avg_time:.3f} s/frame")
+print(f"Average FPS : {1/avg_time:.3f}")
