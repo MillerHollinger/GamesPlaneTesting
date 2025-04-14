58458bf08dc3bad10e66f66a558a0d504d083338
Josh Zhang <joshzhang@Joshs-MacBook-Pro.local>
2025-04-13 20:26:37 -0700

    livestream option and error checking

--- Testing_Video.py
+++ Testing_Video.py
@@ -21,9 +21,10 @@ from Games.DummyGame import *
 
 YAML = "good_calibration.yaml"
 
+#ARGUMENT PARSE
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str, required=True,
-	help="Path to video with ARUCO tag(s)")
+ap.add_argument("-v", "--video", type=str, default="LIVE",
+	help="Path to video. Blank for webcam")
 ap.add_argument("-s", "--scale", type=float, default=1.0,
 	help="Scale factor s*(x,y) for frames")
 ap.add_argument("-d", "--delay", type=int, default=1,
@@ -32,8 +33,8 @@ ap.add_argument("-d", "--delay", type=int, default=1,
 args = vars(ap.parse_args())
 name = args["video"]
 scale= args["scale"]
-print("Processing video " + name)
 
+# RESCALE CALIB
 if scale != 1.0:
     with open(YAML, "r") as file:
         calib = yaml.safe_load(file)
@@ -48,35 +49,51 @@ if scale != 1.0:
     with open(YAML, "w") as file:
         yaml.dump(calib, file)
 
-
+# CAMERA WINDOW
 game = DummyGame(YAML)
 print(f"Starting instance of {game.name}")
 cv2.namedWindow(name, cv2.WINDOW_NORMAL)
-video = cv2.VideoCapture(name)
-times = []
 
-while True:
+if name == "LIVE":
+    print("Accessing live camera...")
+    video = cv2.VideoCapture(0)
+else:
+    print("Processing video " + name)
+    video = cv2.VideoCapture(name)
+
+# PROCESS VIDEO
+times, ret = [], 1
+while ret is not None:
+    # If no viable frames in video, ends loop
+
     start_time = time.time()                # TIMER START
     ret, image = video.read()
-    if not ret: break
     if scale != 1.0:
         image = cv2.resize(image, None, fx=scale, fy=scale)
-
-    pieces, anchors = game.process_image(image)
-    for info in pieces + anchors:
-        info.put_summary_graphic(image)
-        info.put_bounds(image)
-    # print(game.to_board(pieces))
+    
+    # If no viable arucos in frame, try again
+    try:
+        pieces, anchors = game.process_image(image)
+        for info in pieces + anchors:
+            info.put_summary_graphic(image)
+            info.put_bounds(image)
+        # print(game.to_board(pieces))
+    except Exception:
+        print("No viable arucos in frame.")
     
     cv2.imshow(name, image)
     cv2.waitKey(args["delay"])
     times.append(time.time() - start_time)  # TIMER END
 
+# PROCESS STATS
 video.release()
 cv2.destroyAllWindows()
 
+if len(times) == 0:
+    raise Exception("No viable frames in video.")
 avg_time = sum(times) / len(times)
-print(f"Total time  : {sum(times):.3f} secs")
-print(f"Total frames: {len(times)} frames")
+
+print(f"Total time  : {sum(times):.3f} seconds")
+print(f"Total frames: {len(times):.3f} frames")
 print(f"Average time: {avg_time:.3f} s/frame")
-print(f"Average FPS : {1/avg_time:.3f}")
+print(f"Average FPS : {1/avg_time:.3f} fps")
