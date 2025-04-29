1611844f0e6c9fcd39a33a4d9f6fce133797c12d
Josh Zhang <joshzhang@berkeley.edu>
2025-04-29 14:48:02 -0700

    refactored demowithfetch, added state estimation

--- Games/GamesPlaneGame.py
+++ Games/GamesPlaneGame.py
@@ -3,7 +3,8 @@ from Helpers.PhysicalBoardInfo import *
 from Helpers.PhysicalAruco import *
 
 class GamesPlaneGame:
-    def __init__(self, name: str, anchored_arucos: list[PhysicalAruco], unanchored_arucos: list[PhysicalAruco], valid_positions: list[tuple[float, float]], cm_to_space: float, camera_yaml: str):
+    def __init__(self, name: str, anchored_arucos: list[PhysicalAruco], unanchored_arucos: list[PhysicalAruco], 
+                 valid_positions: list[tuple[float, float]], cm_to_space: float, camera_yaml: str):
         self.name = name
         self.board_info = PhysicalBoardInfo(
             unanchored_arucos,
@@ -16,8 +17,8 @@ class GamesPlaneGame:
         return
 
     # Shortcut to pass an image to the gframe.
-    def process_image(self, image):
-        return self.gframe.process_image(image)
+    def process_image(self, image, reason=True):
+        return self.gframe.process_image(image, reason)
     
     # DEBUG Helper for now. Returns a text-based representation of the board.
     def to_board(self, pieces: list[DigitalAruco]) -> str:
